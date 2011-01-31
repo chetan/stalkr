08dc75db3d554f2c847378b0c143fe0c0e672155
Chetan Sarva <chetan@pixelcop.net>
2011-01-30 21:00:22 -0500

    fixed indent

--- lib/stalkr/ups.rb
+++ lib/stalkr/ups.rb
@@ -18,62 +18,62 @@ class UPS < Base
 
     self.regex = /\b(1Z ?[0-9A-Z]{3} ?[0-9A-Z]{3} ?[0-9A-Z]{2} ?[0-9A-Z]{4} ?[0-9A-Z]{3} ?[0-9A-Z]|[\dT]\d{3} ?\d{4} ?\d{3})\b/i
 
-def track(id)
+    def track(id)
 
-    url = "http://wwwapps.ups.com/WebTracking/processInputRequest?TypeOfInquiryNumber=T&loc=en_US&InquiryNumber1=%CODE%"
+        url = "http://wwwapps.ups.com/WebTracking/processInputRequest?TypeOfInquiryNumber=T&loc=en_US&InquiryNumber1=%CODE%"
 
-    url.gsub!(/%CODE%/, id)
-    html = fetchurl(url)
+        url.gsub!(/%CODE%/, id)
+        html = fetchurl(url)
 
-    detail_scraper = Scraper.define do
+        detail_scraper = Scraper.define do
 
-        array :keys
-        array :vals
-        array :lists
+            array :keys
+            array :vals
+            array :lists
 
-        process "#trkNum", :trackingNumber => :text
-        process "#tt_spStatus", :status => :text
-        process "#fontControl dt", :keys => :text
-        process "#fontControl dd", :vals => :text
-        process "#fontControl ul.clearfix li", :lists => :text
+            process "#trkNum", :trackingNumber => :text
+            process "#tt_spStatus", :status => :text
+            process "#fontControl dt", :keys => :text
+            process "#fontControl dd", :vals => :text
+            process "#fontControl ul.clearfix li", :lists => :text
 
-        result :keys, :vals, :trackingNumber, :status, :lists
+            result :keys, :vals, :trackingNumber, :status, :lists
+
+        end
+
+        details = detail_scraper.scrape(html)
+
+        if not details.trackingNumber then
+            raise "UPS scraper failed"
+        end
+
+        ret = Result.new(:UPS)
+
+        ret.status = case details.status.strip.downcase
+            when "in transit"
+                IN_TRANSIT
+            when "delivered"
+                DELIVERED
+            else
+                UNKNOWN
+        end
+
+        hash = {}
+        details.keys.each_with_index do |k,i|
+            hash[k] = details.vals[i]
+        end
+
+        if ret.status == DELIVERED then
+            delivered_at = cleanup_html( hash["Delivered On:"] )
+            ret.delivered_at = DateTime.strptime( delivered_at, "%A, %m/%d/%Y at %I:%M %p" ).to_time
+        end
+
+        cleanup_html( details.lists[3] ) =~ /Updated: (.*?)$/
+        ret.updated_at = DateTime.strptime( $1, "%m/%d/%Y %I:%M %p" ).to_time
+
+        return ret
 
     end
-
-    details = detail_scraper.scrape(html)
-
-    if not details.trackingNumber then
-        raise "UPS scraper failed"
-    end
-
-    ret = Result.new(:UPS)
-
-    ret.status = case details.status.strip.downcase
-        when "in transit"
-            IN_TRANSIT
-        when "delivered"
-            DELIVERED
-        else
-            UNKNOWN
-    end
-
-    hash = {}
-    details.keys.each_with_index do |k,i|
-        hash[k] = details.vals[i]
-    end
-
-    if ret.status == DELIVERED then
-        delivered_at = cleanup_html( hash["Delivered On:"] )
-        ret.delivered_at = DateTime.strptime( delivered_at, "%A, %m/%d/%Y at %I:%M %p" ).to_time
-    end
-
-    cleanup_html( details.lists[3] ) =~ /Updated: (.*?)$/
-    ret.updated_at = DateTime.strptime( $1, "%m/%d/%Y %I:%M %p" ).to_time
-
-    return ret
-
-end
 
 end # class UPS
 end # module Stalkr
